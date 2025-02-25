865e11b5e3d21577076c0c0b731e7100e0ffe3d1
AidanThadAnd <aidanandrews112@gmail.com>
2025-02-25 13:29:48 -0700

    fixes done

--- node/js/artistsRoutes.js
+++ node/js/artistsRoutes.js
@@ -8,12 +8,14 @@ router.get("/:ref", (req, res) =>
   handleRPC("artist_by_ref", { artist_id: req.params.ref }, res)
 );
 
-router.get("/lastname/:lastname", (req, res) =>
+router.get("/search/:lastname", (req, res) =>
   handleRPC("get_artists_by_lastname", { lname: req.params.lastname }, res)
 );
 
-router.get("/nationality/:national", (req, res) =>
+router.get("/country/:national", (req, res) =>
   handleRPC("get_artists_by_nationality", { nation: req.params.national }, res)
 );
+
+
 
 module.exports = router;
